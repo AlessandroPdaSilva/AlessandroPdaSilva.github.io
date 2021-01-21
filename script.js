cb5cdd034ecc4bda655a0162cf50c8aab815d714
Alessandro Pereira <73945572+AlessandroPdaSilva@users.noreply.github.com>
2021-01-21 12:02:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function clicado(){
+    // variaveis
+    $botao = document.getElementById("btn")
+    $modal = document.getElementById("modal")
+
+    $modal.style.bottom = "0%"
+    
+
+}
+
+function desfazer(){
+    $botao = document.getElementById("btn")
+    $modal = document.getElementById("modal")
+
+    $modal.style.bottom = "-100%"
+}
+
+
+
+
+
+
+
+
+
